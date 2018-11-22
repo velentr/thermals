ed61f132abfb07cbf3d1f63d0dcbafe642379c86
Brian Kubisiak <brian@kubisiak.com>
2018-11-22 11:54:28 -0800

    Add additional fault testing.
    Additional test verifies that the output is ramped up to 1.0 after a
    fault is detected and that the hi output warning is reported to the
    event manager.

--- test/thermals_SUITE.erl
+++ test/thermals_SUITE.erl
@@ -5,11 +5,15 @@
 -behavior(gen_event).
 
 -export([all/0, end_per_testcase/2, init_per_testcase/2]).
--export([absent_hwmon_temp/1, check_fault/1]).
+-export([absent_hwmon_temp/1, check_fault/1, ramp_up_after_fault/1]).
 -export([handle_call/2, handle_event/2, init/1]).
 
-all() -> [absent_hwmon_temp, check_fault].
+all() -> [absent_hwmon_temp, check_fault, ramp_up_after_fault].
 
+init_per_testcase(ramp_up_after_fault, Config) ->
+    ok = application:set_env(thermals, threshold, {0.5, 0.7}),
+    {ok, _Started} = application:ensure_all_started(thermals),
+    Config;
 init_per_testcase(_, Config) ->
     {ok, _Started} = application:ensure_all_started(thermals),
     Config.
@@ -31,9 +35,17 @@ check_fault(_Config) ->
     {ok, _Pid} = thermals:spawn(fault, [input, output]),
     ok = wait_for_fault(5000).
 
+ramp_up_after_fault(_Config) ->
+    ok = thermals:watch_sup(?MODULE, self()),
+    {ok, _Pid} = thermals:spawn(fault, [input, output]),
+    ok = wait_for_fault(5000),
+    {ok, 1.0} = wait_for_hi(5000).
+
 handle_event({fault, _Group, _Reason}, Pid) ->
     Pid ! fault,
     {ok, Pid};
+handle_event({warn, _Group, {hi, Value}}, Pid) ->
+    Pid ! {hi, Value};
 handle_event(_Ev, Pid) ->
     {ok, Pid}.
 
@@ -43,6 +55,14 @@ init(Pid) -> {ok, Pid}.
 wait_for_fault(Timeout) ->
     receive
 	fault -> ok
+    after Timeout ->
+	    {error, timeout}
+    end.
+
+wait_for_hi(Timeout) ->
+    receive
+	{hi, Value} ->
+	    {ok, Value}
     after Timeout ->
 	    {error, timeout}
     end.
