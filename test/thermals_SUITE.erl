272164a398478e15b6689ca14f1c7873f2ed457b
Brian Kubisiak <brian@kubisiak.com>
2018-11-22 18:42:05 -0800

    Add additional hwmon tests.
    Tests include verifying that too high/too low values are properly
    saturated before being reported.

--- test/thermals_SUITE.erl
+++ test/thermals_SUITE.erl
@@ -8,18 +8,20 @@
 
 -export([all/0, end_per_testcase/2, init_per_testcase/2]).
 -export([absent_hwmon_temp/1, check_fault/1, hwmon_static/1,
-	 ramp_up_after_fault/1]).
+	 hwmon_over/1, hwmon_under/1, ramp_up_after_fault/1]).
 -export([handle_call/2, handle_event/2, init/1]).
 
-all() -> [absent_hwmon_temp, check_fault, hwmon_static,
-	  ramp_up_after_fault].
+all() -> [absent_hwmon_temp, check_fault, hwmon_static, hwmon_over,
+	  hwmon_under, ramp_up_after_fault].
 
 init_per_testcase(ramp_up_after_fault, Config) ->
     ok = application:set_env(thermals, threshold, {0.5, 0.7}),
     {ok, _Started} = application:ensure_all_started(thermals),
     Config;
-init_per_testcase(hwmon_static, Config) ->
-    ok = application:set_env(thermals, threshold, {0.3, 0.4}),
+init_per_testcase(Test, Config) when Test =:= hwmon_static
+				     orelse Test =:= hwmon_over
+				     orelse Test =:= hwmon_under ->
+    ok = application:set_env(thermals, threshold, {-1, -0.5}),
     {ok, _Started} = application:ensure_all_started(thermals),
     Config;
 init_per_testcase(_, Config) ->
@@ -50,6 +52,22 @@ hwmon_static(Config) ->
 				 {path, ?config(data_dir, Config)},
 				 {index, 1}]),
     {ok, 0.5} = wait_for_hi(5000).
+
+hwmon_over(Config) ->
+    ok = thermals:watch_sup(?MODULE, self()),
+    {ok, _Pid} = thermals:spawn(hwmon_temp,
+				[{min, 30}, {max, 40.0},
+				 {path, ?config(data_dir, Config)},
+				 {index, 2}]),
+    {ok, 1.0} = wait_for_hi(5000).
+
+hwmon_under(Config) ->
+    ok = thermals:watch_sup(?MODULE, self()),
+    {ok, _Pid} = thermals:spawn(hwmon_temp,
+				[{min, 30}, {max, 40.0},
+				 {path, ?config(data_dir, Config)},
+				 {index, 3}]),
+    {ok, 0.0} = wait_for_hi(5000).
 
 ramp_up_after_fault(_Config) ->
     ok = thermals:watch_sup(?MODULE, self()),
