19640a6150f77a32a12360fae93175a8548036e8
Brian Kubisiak <brian@kubisiak.com>
2018-11-29 19:38:35 -0800

    Add sup to dialyzer.

--- src/thermals_sup.erl
+++ src/thermals_sup.erl
@@ -4,9 +4,14 @@
 -export([start_link/1]).
 -export([init/1]).
 
+-spec start_link(Params :: map()) ->
+			{ok, pid()} | {error, Reason :: term()}.
 start_link(Params) when is_map(Params) ->
     supervisor:start_link(?MODULE, Params).
 
+-spec init(Params :: map()) ->
+		  {ok, {supervisor:sup_flags(),
+			[supervisor:child_spec()]}}.
 init(Params) ->
     {ok, {#{strategy => one_for_one},
 	  [#{id => thermals_controller,
